b8d9c385f5907c844a5435d04254459e6446bb00
Eshan Das <eshandasnit@gmail.com>
2018-10-29 14:36:38 +0530

    Fixed minor issues

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     name='djello',
     packages=find_packages(),
     include_package_data=True,
-    version='1.0',
+    version='1.0.a',
     description='A pretty and configurable admin panel to enhance the default Django Admin Panel based on Ela Admin',
     author='Eshan Das',
     author_email='eshandasnit@gmail.com',
     url='https://github.com/eshandas/djello',  # use the URL to the github repo
-    download_url='https://github.com/eshandas/djello/archive/1.0.tar.gz',  # Create a tag in github
+    download_url='https://github.com/eshandas/djello/archive/1.0.a.tar.gz',  # Create a tag in github
     keywords=['django', 'admin', 'panel', 'dashboard'],
     classifiers=[],
     install_requires=[
